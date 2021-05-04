c55673714607f9ec2b69b8e895fc42268b941e82
ssubhankar1 <ssubhankar1@gmail.com>
2021-05-04 07:32:40 +0530

    Minor Changes

--- trade-system/src/main/java/com/tradesystem/tradesystem/TradeSystemApplication.java
+++ trade-system/src/main/java/com/tradesystem/tradesystem/TradeSystemApplication.java
@@ -2,10 +2,12 @@ package com.tradesystem.tradesystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "com.tradesystem.tradesystem")
 @SpringBootApplication
+//@EnableEurekaClient
 public class TradeSystemApplication {
 
 	public static void main(String[] args) {
